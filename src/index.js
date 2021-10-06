694fdf35bd424b2dee99a76151c028cd6f9ef67d
Gustavo Fonseca <44001877+fonsecagustavo@users.noreply.github.com>
2021-10-06 20:59:30 -0300

    Termino

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate: validateUUID } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json();
+  }
+
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro ){
+    if(10 <= user.todos.length){
+      return response.status(403).json();
+    }
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!validateUUID(id)){
+    return response.status(400).json();
+  }
+
+  if(!user){
+    return response.status(404).json();
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json();
+  }
+
+  request.user = user;
+  request.todo = todo;
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id );
+
+  if(!user) {
+    return response.status(404).json();
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
